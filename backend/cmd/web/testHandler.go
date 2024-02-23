ea8fcd9c3af48c05f42c8c1ed68ff06051a52dd2
Nevezhin Yan <yaniknezhin@gmail.com>
2024-02-23 14:10:04 +0600

    Testing

--- backend/cmd/web/testHandler.go
+++ backend/cmd/web/testHandler.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Привет!"))
+	fmt.Println("Топ 5 смешных пул реквестов...")
 }
