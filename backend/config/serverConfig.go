acca84efc81a2576e97ad9b890d2ea0297c84931
Nevezhin Yan <yaniknezhin@gmail.com>
2024-03-05 20:17:04 +0600

    some config logic add

---
+++ backend/config/serverConfig.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+type ServerConfig struct {
+	Port         int
+	Handler      http.Handler
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+func InitServerConfig(hndr http.Handler) (*ServerConfig, error) {
+	port, err := strconv.Atoi(os.Getenv("SERVER_PORT"))
+	readTimeout, err := strconv.Atoi(os.Getenv("SERVER_READ_TIMEOUT"))
+	writeTimeout, err := strconv.Atoi(os.Getenv("SERVER_WRITE_TIMEOUT"))
+	if err != nil {
+		return nil, err
+	}
+	return &ServerConfig{
+		Port:         port,
+		Handler:      hndr,
+		ReadTimeout:  time.Duration(readTimeout) * time.Second,
+		WriteTimeout: time.Duration(writeTimeout) * time.Second,
+	}, nil
+}
